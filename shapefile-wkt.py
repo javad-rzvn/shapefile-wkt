5b58ff2c1c0628dbdd373e499c0e46e06472bfdd
javad <javad_design@yahoo.com>
2025-01-31 16:46:42 +0330

    Change: update main file

--- shapefile-wkt.py
+++ shapefile-wkt.py
@@ -19,42 +19,47 @@ def _(mo):
 
 
 @app.cell
-def _(gpd, shapefiles):
-    shapefile_df = {}
-    for item in shapefiles.value:
-        df = gpd.read_file(item.contents)
-        shapefile_df.update({item.name: df})
-    return df, item, shapefile_df
+def _(shapefiles):
+    shapefiles.value
+    return
 
 
 @app.cell
-def _(mo, shapefile_df):
-    # mo.ui.table(shapefile_df[0], page_size=5)
-    preview_tabs = {file_name:mo.ui.table(file_content, page_size=5) for file_name, file_content in shapefile_df.items()}
-    mo.ui.tabs(preview_tabs)
-    return (preview_tabs,)
+def _():
+    # shapefile_df = {}
+    # for item in shapefiles.value:
+    #     df = gpd.read_file(item.contents)
+    #     shapefile_df.update({item.name: df})
+    return
 
 
 @app.cell
-def _(mo, shapefile_df):
-    shapefile_columns = {}
-    for fname, fcontent in shapefile_df.items():
-        file_cols = {
-            fname: {
-                col_name: mo.ui.text(value=col_name) for col_name in fcontent.columns
-            }
-        }
-        shapefile_columns.update(file_cols)
-
-    mo.ui.tabs(shapefile_columns)
-    return fcontent, file_cols, fname, shapefile_columns
+def _():
+    # preview_tabs = {file_name:mo.ui.table(file_content, page_size=5) for file_name, file_content in shapefile_df.items()}
+    # mo.ui.tabs(preview_tabs)
+    return
 
 
 @app.cell
-def _(shapefile_columns):
-    te = shapefile_columns["plain.zip"]["OBJECTID_1"]
-    print(te.value)
-    return (te,)
+def _():
+    # shapefile_columns = {}
+    # for fname, fcontent in shapefile_df.items():
+    #     file_cols = {
+    #         fname: {
+    #             col_name: mo.ui.text(value=col_name) for col_name in fcontent.columns
+    #         }
+    #     }
+    #     shapefile_columns.update(file_cols)
+
+    # mo.ui.tabs(shapefile_columns)
+    return
+
+
+@app.cell
+def _():
+    # te = shapefile_columns["plain.zip"]["OBJECTID_1"]
+    # print(te.value)
+    return
 
 
 if __name__ == "__main__":
